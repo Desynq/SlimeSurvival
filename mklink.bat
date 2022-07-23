97e57d38f5e2fd76f5a2dbd20558cc0ac28d127f
Desynq <69401694+Desynq@users.noreply.github.com>
2022-07-23 13:06:11 -0400

    :camera: Snapshot

--- mklink.bat
+++ mklink.bat
@@ -1,3 +1,6 @@
 @ECHO OFF
 
-mklink /J "C:\Users\notde\OneDrive\Documents\GitHub\SlimeSurvival\run\saves\Testing\scripts" "D:\Servers\Slime Survival\1.18.2 Modded\world_slime_survival\scripts"
+cd "C:\Users\notde\OneDrive\Documents\GitHub\SlimeSurvival\src\main\resources"
+
+mklink /J "C:\Users\notde\curseforge\minecraft\Instances\Slime Survival 1.18.2\global_packs\required_data\temp\data" "data"
+mklink /J "C:\Users\notde\curseforge\minecraft\Instances\Slime Survival 1.18.2\global_packs\required_resources\temp\assets" "assets"
