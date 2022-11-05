c65c1347481ac473cfb9570b59b6152181ba200e
Desynq <69401694+Desynq@users.noreply.github.com>
2022-11-05 03:06:32 -0400

    Renamed `ModAttributes` class Now is `ModEntityAttributes` to make its contents more concise.

--- src/main/java/net/slimesurvival/common/registry/ModEntityAttributes.java
+++ src/main/java/net/slimesurvival/common/registry/ModEntityAttributes.java
@@ -5,7 +5,7 @@ import net.minecraft.entity.attribute.EntityAttribute;
 import net.minecraft.util.registry.Registry;
 import net.slimesurvival.SlimeSurvival;
 
-public class ModAttributes {
+public class ModEntityAttributes {
 
 	public static final EntityAttribute DODGE_CHANCE_ATTRIBUTE = Registry.register(Registry.ATTRIBUTE, SlimeSurvival.ID("dodge_chance"), new ClampedEntityAttribute("attribute.name.slimesurvival.dodge_chance", 0.00, 0.0, 1.0));
 	
