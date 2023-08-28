ff731ded6de4cf4839566d231449d75f24242037
Desynq <69401694+Desynq@users.noreply.github.com>
2023-08-28 17:09:58 -0400

    Added identifier method thing to mod

--- src/main/java/slimesurvival/SlimeSurvival.java
+++ src/main/java/slimesurvival/SlimeSurvival.java
@@ -1,11 +1,21 @@
 package slimesurvival;
 
 import net.fabricmc.api.ModInitializer;
+import net.minecraft.util.Identifier;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class SlimeSurvival implements ModInitializer {
+
+	public static final String MOD_ID = "slimesurvival";
+	public static final String MOD_NAME = "Slime Survival";
+
+	public static Identifier ID(String path) {
+		return new Identifier(MOD_ID, path);
+	}
+
+
 	// This logger is used to write text to the console and the log file.
 	// It is considered best practice to use your mod id as the logger's name.
 	// That way, it's clear which mod wrote info, warnings, and errors.
