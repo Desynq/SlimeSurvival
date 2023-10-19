62701561a0ce8392be2d323a54a2544bc6c3148c
Desynq <69401694+Desynq@users.noreply.github.com>
2023-10-18 20:40:19 -0400

    Changed dodge chance attribute namespace

--- src/main/java/slimesurvival/common/registry/ModEntityAttributes.java
+++ src/main/java/slimesurvival/common/registry/ModEntityAttributes.java
@@ -7,12 +7,13 @@ import net.minecraft.registry.Registry;
 import slimesurvival.SlimeSurvival;
 
 public class ModEntityAttributes {
-	public static final EntityAttribute DODGE_CHANCE = createAttribute("dodge_chance", 0, 0, 1);
+	public static final EntityAttribute DODGE_CHANCE = createAttribute("dodge_chance", 0, 0, Integer.MAX_VALUE);
+	public static final EntityAttribute CHANCE_TO_NOT_CONSUME_AMMO = createAttribute("chance_to_not_consume_ammo", 0, 0, Integer.MAX_VALUE);
 
 
 
 	private static EntityAttribute createAttribute(final String name, double base, double min, double max) {
-		return new ClampedEntityAttribute("attribute.name.generic." + SlimeSurvival.MOD_ID + "." + name, base, min, max).setTracked(true);
+		return new ClampedEntityAttribute("attribute.name." + SlimeSurvival.MOD_ID + "." + name, base, min, max).setTracked(true);
 	}
 
 
